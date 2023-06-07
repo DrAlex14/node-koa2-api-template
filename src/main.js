b28ffb63c4b70fdb8b29c933f4c70566f4ff5145
zzy <975722423@qq.com>
2023-06-07 23:04:32 +0800

    添加路由

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 const Koa = require('koa')
 const {APP_PORT} = require('../config/config.default.js')
+const userRouter = require('./router/user.router.js')
 
 const app = new Koa()
 
-app.use((ctx, next) => {
-    ctx.body = 'hello world'
-})
+
+app.use(userRouter.routes())
 
 app.listen(APP_PORT, (ctx) => {
-    console.log('server is running on http://localhost:8000');
+    console.log(`server is running on http://localhost:${APP_PORT}`);
 })
 
 
